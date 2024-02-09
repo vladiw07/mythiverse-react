bdba0ba4ac2a06a56fb8e58a81dc40fe5f19880b
vladiw07 <onevlade44@gmail.com>
2024-02-09 23:44:24 +0200

    Description of changes

--- src/components/pages/Mythologies/GreekMythology.jsx
+++ src/components/pages/Mythologies/GreekMythology.jsx
@@ -5,13 +5,17 @@ function GreekMythology() {
   return (
     <>
     <div className="image-start-wrapper">
-    <div className='imageStart'></div>
+    <div className='imageStart'>
+
     <h1>Ultimate Guide to Greek Mythology</h1>
         <p>Greek mythology is the body of work detailing the
         origins of the ancient Hellenic world and the many
         deities who ruled over it. <br /> It includes the histories
         of gods and goddesses, heroes and monsters, as well
         as explanations for many important religious rituals.</p>
+
+    </div>
+    
     </div>
     
     
