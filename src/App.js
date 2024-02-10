e48525b0e729c0985a665b7b69bc52ea9cd2a019
vladiw07 <onevlade44@gmail.com>
2024-02-10 17:33:52 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,38 @@ import About from './components/pages/main/About'
 import Contact from './components/pages/main/Contact'
 import Home from "./components/pages/main/Home";
 import MythologyTemplate from "./components/pages/Mythologies/MythologyTemplate";
-import greekMythology from './components/pages/Mythologies/images/greekMythology.avif'
+import greekMythology from './components/pages/Mythologies/images/greekMythology.jpg'
 import norseMythology from './components/pages/Mythologies/images/norseMythology.jpg'
+import secondGreekGodsImage from './components/pages/Mythologies/images/greekGodsImage.jpg'
+import secondNorseGodsImage from './components/pages/Mythologies/images/norseGodsImage.avif'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-      <Route path="/greek-mythology" element={<MythologyTemplate imageUrl={greekMythology} header={'Ultimate Guide to Greek Mythology'} firstText={<p>Greek mythology is the body of work detailing the origins of the ancient Hellenic world and the many deities who ruled over it.It includes the histories of gods and goddesses, heroes and monsters, as well as explanations for many important religious rituals.</p>} />} />
-      <Route path="/norse-mythology" element={<MythologyTemplate imageUrl={norseMythology} header={'Ultimate Guide to Norse Mythology'} firstText={<p>Norse mythology is the compilation of the stories, religious practices, and myths of the ancient Nordic people. These texts span a period of time beginning in Norse paganism and continuing through the medieval era, during which most of Scandinavia converted to Christianity.</p>} />} />
+      <Route 
+      path="/greek-mythology" 
+      element={<MythologyTemplate 
+      imageUrl={greekMythology}
+      secondImageUrl={secondGreekGodsImage}
+      header={'Ultimate Guide to Greek Mythology'} 
+      firstText={<p>Greek mythology is the body of work detailing the origins of the ancient Hellenic world and the many deities who ruled over it.It includes the histories of gods and goddesses, heroes and monsters, as well as explanations for many important religious rituals.</p>} 
+      godsHeader={'Greek Gods'} godText={<p>The Greek gods ruled over every aspect of Hellenic existence—from war to love, from childbirth to the afterlife. Commonly depicted in human form, they were capriciou s deities who demanded worship and sacrifice  to stave off misfortune and ruin.</p>} 
+      godLiElements={<ul className="godsUl"><li>Greek Olympians</li><li>Athena</li><li>Poseidon</li><li>Greek Titans</li><li>Zeus</li><li>Prometheus</li></ul>} />} 
+      />
+
+      <Route 
+      path="/norse-mythology" 
+      element={<MythologyTemplate 
+      imageUrl={norseMythology} 
+      secondImageUrl={secondNorseGodsImage}
+      header={'Ultimate Guide to Norse Mythology'} 
+      firstText={<p>Norse mythology is the compilation of the stories, religious practices, and myths of the ancient Nordic people. These texts span a period of time beginning in Norse paganism and continuing through the medieval era, during which most of Scandinavia converted to Christianity.</p>} 
+      godsHeader={'Nordic Gods'}
+      godText={<p>The Norse gods and goddesses are the array of deities honored by ancient Nordic worshipers. They primarily came from two different tribes, the Aesir and the Vanir, but were united in their efforts to fight the jötnar, a tribe of giants dwelling in another realm of the world tree Yggdrasil.</p>}
+      godLiElements={<ul className="godsUl"><li>Odin</li><li>Loki</li><li>Frigg</li><li>Freya</li><li>Thor</li><li>Baldur</li></ul>} />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
--- src/components/pages/Mythologies/MythologyTemplate.jsx
+++ src/components/pages/Mythologies/MythologyTemplate.jsx
@@ -3,16 +3,32 @@ import "./MythologyTemplate.css";
 
 
 
-function MythologyTemplate( { imageUrl, header, firstText } ) {
+function MythologyTemplate( { imageUrl, secondImageUrl, header, firstText, godsHeader, godText, godLiElements } ) {
   return (
     <>
     <div>
     <div style={{ background: `url(${imageUrl})` }} className='imageStart'>
     <h1>{header}</h1>
         {firstText}
-    </div>
+        <button className="discover-button">Discover More</button>
     </div>
     
+
+    <div className="god-section-plus-image-wrapper">
+      
+      <div style={{ background: `url(${secondImageUrl})` }} className='firstSubImage'></div>
+     
+      
+    <div className="Gods-section">
+      <header>{godsHeader}</header>
+      {godText}
+      <button>View Collection</button>
+      <h2>Popular Resources</h2>
+      {godLiElements}
+      </div>
+    </div>
+    </div>
+   
     
     
     </>
