03a1f9def6b93fd5d5035f3020bee8e787eda491
vladiw07 <onevlade44@gmail.com>
2024-02-29 19:11:12 +0200

    main

--- src/components/mainPageParts/AllMythologiesSection.jsx
+++ src/components/mainPageParts/AllMythologiesSection.jsx
@@ -1,41 +1,57 @@
 import React from 'react'
 import "./AllMythologiesSection.css"
 
+
 function AllMythologiesSection() {
   return (
     <>
+    <div id='headers'>
+    <h2>Spotlight</h2>
+    <h1>Meet the Greeks</h1>
+    <hr />
+    </div>
       <ul className='mythologiesUl'>
         <li>
-        <header>Aztec Mythology</header>
+        <div className="img"></div>
+        <p id='collectiontext'>collection</p>
+        <header>Greek Olympians</header>
+        <p>The primary deities of the Greek pantheon, who ruled from Mount Olympus</p>
+        <button>View</button>
+        <hr />
+        <p className="featuredP">featured</p>
         </li>
         
         <li>
-        <header>Celtic Mythology</header>
+        <div className="img"></div>
+        <p id='collectiontext'>collection</p>
+        <header>Greek Heroes</header>
+        <p>Notable or superhuman mortals from Greek mythology</p>
+        <button>View</button>
+        <hr />
+        <p className="featuredP">featured</p>
         </li>
         
         <li>
-        <header>Chinese Mythology</header>
+        <div className="img"></div>
+        <p id='collectiontext'>collection</p>
+        <header>Greek Creatures</header>
+        <p>Legendary beasts, animals, and humanoids of ancient Greek mythology</p>
+        <button>View</button>
+        <hr />
+        <p className="featuredP">featured</p>
+        </li>
+
+        <li>
+        <div className="img"></div>
+        <p id='collectiontext'>collection</p>
+        <header>Greek Titans</header>
+        <p>The generation of Greek gods who directly preceded the Olympians</p>
+        <button>View</button>
+        <hr />
+        <p className="featuredP">featured</p>
         </li>
        
-        <li>
-        <header>Egyptian Mythology</header>
-        </li>
         
-        <li>
-        <header>Greek Mythology</header>
-        </li>
-
-        <li>
-        <header>Hindu Mythology</header>
-        </li>
-
-        <li>
-        <header>Japanese Mythology</header>
-        </li>
-
-        <li>
-        <header>Norse Mythology</header>
-        </li>
 
       </ul>
     </>
